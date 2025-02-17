a1f4efeb26c8ccf5afa1ca738d0489846763519b
danielace1 <danielsudharsan1805@gmail.com>
2025-02-17 12:53:04 +0530

    added privacy policy link

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,14 +1,22 @@
 const Footer = () => {
   return (
-    <footer className="py-3 md:py-4 text-center text-sm text-white bg-gradient-to-r from-blue-500/50 to-indigo-500/65 backdrop-blur-lg">
-      Made with ❤️ by
+    <footer className="py-3 md:py-4 text-sm text-white bg-gradient-to-r from-blue-500/50 to-indigo-500/65 backdrop-blur-lg flex items-center justify-center relative">
+      <div>
+        Made with ❤️ by
+        <a
+          href="https://github.com/danielace1"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-semibold hover:underline ml-1 text-gray-100 cursor-pointer"
+        >
+          Sudharsan
+        </a>
+      </div>
       <a
-        href="https://github.com/danielace1"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="font-semibold hover:underline ml-1 text-gray-100 cursor-pointer"
+        href="/privacy-policy"
+        className="absolute right-4 text-gray-100 hover:underline"
       >
-        Sudharsan
+        Privacy Policy
       </a>
     </footer>
   );
